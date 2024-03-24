e9fca48c665b68a365e57f52969a60603d8ed094
Chippi <Chippi@users.noreply.github.com>
2024-03-24 22:47:48 +0000

    deploy: Insynskodarna/Insynskungen@716e2ddaa26f64288de57bff50bfe72aea38ec25

--- chunk-XLSAHDQB.js
+++
@@ -1,2 +0,0 @@
-import{Z as a,ab as o,db as i}from"./chunk-QAU7Z64W.js";var p=(()=>{let e=class e{ngOnInit(){}};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=a({type:e,selectors:[["app-app-dashboard"]],standalone:!0,features:[i],decls:1,vars:0,template:function(n,s){n&1&&o(0,`dashboard
-`)},changeDetection:0});let t=e;return t})();export{p as AppDashboardComponent};
