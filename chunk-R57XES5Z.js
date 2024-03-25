bb8af3e131b9cb8443987a3da0407b1222c64099
Chippi <Chippi@users.noreply.github.com>
2024-03-25 21:41:28 +0000

    deploy: Insynskodarna/Insynskungen@d12ce932819911ae358e6876d5863a776c1dc12f

---
+++ chunk-R57XES5Z.js
@@ -0,0 +1,2 @@
+import{Z as o,ab as i,db as a}from"./chunk-O5PVWP7A.js";var s=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-in-app-overview"]],standalone:!0,features:[a],decls:1,vars:0,template:function(n,c){n&1&&i(0,`\xD6versikt
+`)},changeDetection:0});let t=e;return t})();export{s as InAppOverviewComponent};
